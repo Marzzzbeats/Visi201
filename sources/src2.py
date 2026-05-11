f1113389ca25653931cb7a8a66ac069ea8f5f4bd
Marzzzbeats <simonmarzin@gmail.com>
2026-05-11 21:15:47 +0200

    clean

--- sources/src2.py
+++
@@ -1,5 +0,0 @@
-def f(x):
-    y = x + 1
-    return y
-
-print(f(10))
--- sources/src3.py
+++
@@ -1,8 +0,0 @@
-def outer():
-    x = 10
-    def middle():
-        def inner():
-            return x
-        return
-    x = 20
-    return
